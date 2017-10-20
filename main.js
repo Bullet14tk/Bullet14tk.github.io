b6aec4cda60f0500dc6e8de6102d5de5126232dc
Bullet14tk <31598869+Bullet14tk@users.noreply.github.com>
2017-10-20 09:33:47 -0700

    More Java

--- main.js
+++ main.js
@@ -1,8 +1,28 @@
+var list = ["we shall see!",
+			"Yes!",
+			"No",
+			"Maybe",
+			"Certainly",
+			"Ask again later",
+			"Doubtful",
+			"Try again later",
+			"As I see it, yes",
+			"Signs point to yes!"];
+function randomNumber() {
+	return Math.floor (Math.random () * 10); 
+}
+
+//just testing
 function testJs(){
 	alert("test!");
 }
-
+//This function handles clicking of 8 ball imagage
 function shake8Ball(){
 	document.getElementById("magic8ball").src="Magic_eight_ball.png";
-	alert("We shall see!")
+	//alert("We shall see!")
+
+
+	document.getElementById("results").innerHTML=list[randomNumber()];
 }
+
+	//alert(randomNumber());
