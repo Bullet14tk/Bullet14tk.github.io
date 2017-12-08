e8802029248c0e826a965bc2fa0ea5a60c43d104
Bullet14tk <31598869+Bullet14tk@users.noreply.github.com>
2017-12-08 09:25:02 -0800

    Tic Tac Toe
    Almost done

--- main.js
+++ main.js
@@ -42,15 +42,20 @@ function turn(location){
 //handles X and O turn
 function turn(location) {
 
-	//switch between X or O
-	if(character=="X"){
-		character = "O";
-	}
-	else{
-		character = "X";
+		//If no X or O, then draw
+		if(document.getElementById(location).innerHTML == ""){
+		//switch between X or O
+		if(character=="X"){
+			character = "O";
+		}
+		else{
+			character = "X";
+		}
+
+		document.getElementById(location).innerHTML = character; 
 	}
 
-	document.getElementById(location).innerHTML = character; 
+	checkWinner();
 }
 
 //clears X's and O's
@@ -64,6 +69,27 @@ function clearAll(){
 	document.getElementById("r3c1").innerHTML = "";
 	document.getElementById("r3c2").innerHTML = "";
 	document.getElementById("r3c3").innerHTML = "";
+
+}
+
+function checkWinner(){
+
+	var r1c1 = document.getElementById("r1c1").innerHTML;
+	var r1c2 = document.getElementById("r1c2").innerHTML;
+	var r1c3 = document.getElementById("r1c3").innerHTML;
+	var r2c1 = document.getElementById("r2c1").innerHTML;
+	var r2c2 = document.getElementById("r2c2").innerHTML;
+	var r2c3 = document.getElementById("r2c3").innerHTML;
+	var r3c1 = document.getElementById("r3c1").innerHTML;
+	var r3c2 = document.getElementById("r3c2").innerHTML;
+	var r3c3 = document.getElementById("r3c3").innerHTML;
+
+	if(r1c1 == r1c2 && r1c1 == r1c3 && r1c1 != "" ||
+	   r2c1 == r2c2 && r2c1 == r2c3 && r2c1 != "" ||
+	   r3c1 == r3c2 && r3c1 == r3c3 && r3c1 != "" ){
+
+		alert("Winner!");
+	}
 
 
 }
